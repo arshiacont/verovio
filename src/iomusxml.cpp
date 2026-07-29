52a30d92978d35cc89208d7687ae20a591ec1beb
Arshia Cont <arshiacont@antescofo.com>
2026-07-29 10:10:19 +0200

    Revert "Fix hidden MusicXML tuplet notation"
    This reverts commit ea83871861712e8d95ff872c6661a061f898a22b.

--- src/iomusxml.cpp
+++ src/iomusxml.cpp
@@ -4313,10 +4313,6 @@ void MusicXmlInput::ReadMusicXmlTupletStart(const pugi::xml_node &node, const pu
     tuplet->SetNumFormat(ConvertTupletNumberValue(tupletStart.attribute("show-number").as_string()));
     if (HasAttributeWithValue(tupletStart, "show-number", "none")) tuplet->SetNumVisible(BOOLEAN_false);
     tuplet->SetBracketVisible(ConvertWordToBool(tupletStart.attribute("bracket").as_string()));
-    if (HasAttributeWithValue(tupletStart.parent(), "print-object", "no")) {
-        tuplet->SetNumVisible(BOOLEAN_false);
-        tuplet->SetBracketVisible(BOOLEAN_false);
-    }
 }
 
 void MusicXmlInput::ReadMusicXmlBeamStart(const pugi::xml_node &node, const pugi::xml_node &beamStart, Layer *layer)
